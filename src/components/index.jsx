e79bcc20f2ce8a894b6bed94e96da31e58152f82
Arif Billah <98398506+ArifBillah6482@users.noreply.github.com>
2022-11-08 19:20:33 +0600

    Update index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,9 +2,9 @@ import AllBiodata from "./AllBiodata";
 
 export default function MainComponent() {
   const pass = prompt("");
-  if (pass === "606920") {
+  if (pass === "s") {
     return <AllBiodata />;
-  } else if (pass !== "606920") {
+  } else if (pass !== "s") {
     return <p style={{ color: "white" }}>এখানে কোনো তথ্য নেই।</p>;
   } else {
     return <p style={{ color: "white" }}>এখানে কোনো তথ্য নেই।</p>;
